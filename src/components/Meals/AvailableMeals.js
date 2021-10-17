a1cb2b6be4865ebf81fa7db3fc8fcf56b713bccf
chaitak-gorai <chaitakgorai@gmail.com>
2021-10-17 12:17:32 +0530

    messy code

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -28,8 +28,8 @@ const DUMMY_MEALS = [
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
   return (
-    <section className={classes.meals}>
-                                <ul>{mealsList}</ul>
+    <section section className={classes.meals}>
+      <ul>{mealsList}</ul>
     </section>
   );
 };
